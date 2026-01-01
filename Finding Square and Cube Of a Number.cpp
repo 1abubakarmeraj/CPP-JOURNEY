1c723c3fb6eddb48dcb4a0cb2cce2f299627128b
Abubakar Khan <enornimous@gmail.com>
2026-01-01 23:11:51 +0500

    Add files via upload

---
+++ Finding Square and Cube Of a Number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int number;
+cout<<"Enter Number: ";
+cin>>number;
+cout<<"The Square Of "<<number<<" is "<<number*number<<endl;
+cout<<"The Cube Of "<<number<<" is "<<number*number*number<<endl;
+
+
+}
