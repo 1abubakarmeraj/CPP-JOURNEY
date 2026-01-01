fea1a9b009eab006eacc4a5907dcae680685f281
Abubakar Khan <enornimous@gmail.com>
2026-01-01 23:19:51 +0500

    Add files via upload

---
+++ Conversion Of Temperature From Celsius To Faranheit.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+float temperature,faranheit;
+cout<<"Enter Temperature (in Celsius): ";
+cin>>temperature;
+faranheit = (temperature*9/5)+32;
+cout<<"The Temperature in Faranheit is: "<<faranheit<<" F"; 
+}
