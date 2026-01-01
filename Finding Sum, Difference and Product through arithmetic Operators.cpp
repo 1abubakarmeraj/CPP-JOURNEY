7f6aac33929e0cb2cb7c87c6daf2de119780c781
Abubakar Khan <enornimous@gmail.com>
2026-01-01 22:59:36 +0500

    Add files via upload

---
+++ Finding Sum, Difference and Product through arithmetic Operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int numb1,numb2;
+	cout<<"Enter First Number: ";
+	cin>>numb1;
+	cout<<"Enter Second Number: ";
+	cin>>numb2;
+	cout<<"The Sum Of "<<numb1<<" and "<<numb2<<" is "<<numb1+numb2<<endl;
+	cout<<"The Difference Of "<<numb1<<" and "<<numb2<<" is "<<numb1-numb2<<endl;
+	cout<<"The Product Of "<<numb1<<" and "<<numb2<<" is "<<numb1*numb2<<endl;
+	
+}
