2521505d768d7e58b5a6d7ea54b971ba09f4cbf3
Abubakar Khan <enornimous@gmail.com>
2026-01-01 23:05:12 +0500

    Add files via upload

---
+++ Finding Area, Circumference and Diameter Of The Circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+float radius;
+cout<<"Enter Radius Of The Circle: ";
+cin>>radius;
+float area = 3.142*radius*radius;
+float Circumference = 2*3.142*radius;
+float Diameter = 2*radius;
+cout<<"The Area Of The Circle is: "<<area<<endl;
+cout<<"The Circumference Of The Circle is: "<<Circumference<<endl;
+cout<<"The Diameter Of The Circle is: "<<Diameter;
+}
